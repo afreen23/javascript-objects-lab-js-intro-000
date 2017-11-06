3ef67007f339c3f04093c951e804e7bd5e53dce8
Soha Rahman <studentinsha12@gmail.com>
2017-11-06 03:59:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 var recipes={pasta: '1 cup'};
+
 function updateObjectWithKeyAndValue(object,key,value) {
  return Object.assign({},object,{[key]:value}); 
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object,key,value) {
+  object[key]=value;
+  return object;
 }
